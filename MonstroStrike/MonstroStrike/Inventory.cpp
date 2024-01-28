8a0d1287b0c6782a37ed5279c3228477f1f1b235
penyew <penyew@hotmail.com>
2024-01-28 15:23:23 +0800

    Initial setup

---
+++ MonstroStrike/MonstroStrike/Inventory.cpp
@@ -0,0 +1,22 @@
+/*!*****************************************************************************
+\file    Inventory.h
+\author
+\par
+\par     Course:  csd1451s24
+\par     Section: A
+\par     Software Engineering Project 2
+\date    1-27-2024
+
+\brief
+*******************************************************************************/
+
+// ---------------------------------------------------------------------------
+// includes
+#include "Inventory.h"
+
+namespace Inventory
+{
+	void InitInventory()
+	{
+	}
+}
