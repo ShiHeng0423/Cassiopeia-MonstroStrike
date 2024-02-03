20c3d46b80d4c4568e7bfc63674d509bfceec64a
penyew <penyew@hotmail.com>
2024-02-03 15:51:06 +0800

    Revert "Merge branch 'Shi-Heng' into Keith"
    This reverts commit 27c297bb2c8cb1a69db962b042e843266b3df964, reversing
    changes made to 56b4e71ce01c157d4847f80e2b2b3a27a9d8d640.

--- MonstroStrike/MonstroStrike/Level1.cpp
+++ MonstroStrike/MonstroStrike/Level1.cpp
@@ -4,15 +4,11 @@
 #include "TransformMatrix.h"
 #include "Player.h"
 #include <iostream>
-#include "CSVMapLoader.h"
-#include "GridTypesList.h"
-#include "Physics.h"
-#include "CollisionShape.h" //For Verticl + Horizontal collision
 
 AEGfxVertexList* pWhiteSquareMesh1;
-AEGfxVertexList* pWhiteSquareMesh;
 AEGfxTexture* background;
 Player* player;
+
 AEGfxVertexList* pLineMesh;
 
 void Level1_Load()
@@ -33,7 +29,7 @@ void Level1_Load()
 		-0.5f, 0.5f, 0xFFFFFFFF, 0.0f, 0.0f);
 
 	// Saving the mesh (list of triangles) in pMesh
-	pWhiteSquareMesh = AEGfxMeshEnd();
+	pWhiteSquareMesh1 = AEGfxMeshEnd();
 
 
 	AEGfxMeshStart();
@@ -44,48 +40,6 @@ void Level1_Load()
 
 void Level1_Initialize()
 {
-	//Initializing grid data
-	for (s16 rows = 0; rows < MAP_ROW_SIZE; rows++)
-	{
-		for (s16 cols = 0; cols < MAP_COLUMN_SIZE; cols++)
-		{
-			switch (gameMap[rows][cols].symbol) //For checking the map symbol from the csv file
-			{
-			case 0:
-				grids2D[rows][cols].typeOfGrid = EMPTY;
-				break;
-			case 1:
-				grids2D[rows][cols].typeOfGrid = NORMAL_GROUND;
-				break;
-			default:
-				break;
-			}
-		}
-	}
-
-	//For Initializing the grids
-	//Drawing first instance
-	for (s16 rows = 0; rows < MAP_ROW_SIZE; rows++)
-	{
-		for (s16 cols = 0; cols < MAP_COLUMN_SIZE; cols++)
-		{
-			grids2D[rows][cols].rowIndex = rows;
-			grids2D[rows][cols].colIndex = cols;
-
-			if (grids2D[rows][cols].typeOfGrid == NORMAL_GROUND)
-			{
-				InitializeGrid(grids2D[rows][cols]);
-				//AEGfxSetTransform(grids2D[rows][cols].transformation.m);
-				//AEGfxMeshDraw(pMeshYellow, AE_GFX_MDM_TRIANGLES);
-			}
-			else if (grids2D[rows][cols].typeOfGrid == EMPTY) //Maybe can just don't draw at all
-			{
-				InitializeGrid(grids2D[rows][cols]);
-				//AEGfxSetTransform(grids2D[rows][cols].transformation.m);
-				//AEGfxMeshDraw(pMeshGrey, AE_GFX_MDM_TRIANGLES);
-			}
-		}
-	}
 }
 
 void Level1_Update()
@@ -93,7 +47,7 @@ void Level1_Update()
 	PlayerUpdate(*player);
 	if (AEInputCheckTriggered(AEVK_ESCAPE) || 0 == AESysDoesWindowExist())
 	{
-		next = GameStates::Quit;
+		current = GameStates::Quit;
 	}
 
 }
@@ -110,19 +64,19 @@ void Level1_Draw()
 
 	AEGfxTextureSet(background, 0, 0);
 	AEGfxSetTransform(ObjectTransformationMatrixSet(0.f, 0.f, 0.f, 1920.f, 1080.f).m);
-	AEGfxMeshDraw(pWhiteSquareMesh, AE_GFX_MDM_TRIANGLES);
+	AEGfxMeshDraw(pWhiteSquareMesh1, AE_GFX_MDM_TRIANGLES);
 
 	AEGfxTextureSet(player->obj.img.pTex, 0, 0);
 	AEGfxSetTransform(ObjectTransformationMatrixSet(player->obj.pos.x,player->obj.pos.y,0.f, player->obj.img.scale.x, player->obj.img.scale.y).m);
-	AEGfxMeshDraw(pWhiteSquareMesh, AE_GFX_MDM_TRIANGLES);
+	AEGfxMeshDraw(pWhiteSquareMesh1, AE_GFX_MDM_TRIANGLES);
 
 	AEVec2 cam;
 	AEGfxGetCamPosition(&cam.x, &cam.y);
 	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
 	AEGfxSetTransform(ObjectTransformationMatrixSet(cam.x, cam.y , 0.f, AEGfxGetWindowWidth(), 1.f).m);
-	AEGfxMeshDraw(pWhiteSquareMesh, AE_GFX_MDM_TRIANGLES);
+	AEGfxMeshDraw(pWhiteSquareMesh1, AE_GFX_MDM_TRIANGLES);
 	AEGfxSetTransform(ObjectTransformationMatrixSet(cam.x, cam.y, 0.5f * PI, AEGfxGetWindowWidth(), 1.f).m);
-	AEGfxMeshDraw(pWhiteSquareMesh, AE_GFX_MDM_TRIANGLES);
+	AEGfxMeshDraw(pWhiteSquareMesh1, AE_GFX_MDM_TRIANGLES);
 
 }
 
