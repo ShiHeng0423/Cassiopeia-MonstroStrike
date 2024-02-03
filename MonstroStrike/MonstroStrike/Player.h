56b4e71ce01c157d4847f80e2b2b3a27a9d8d640
penyew <penyew@hotmail.com>
2024-02-03 15:12:23 +0800

    Revert "Merge branch 'main' into Keith"
    This reverts commit 9d05f6cdfcafd86397e741e68cf6283b6ecea9e0, reversing
    changes made to a6fadc76be2bc15c7b5334fba35c1180c2ffba06.

--- MonstroStrike/MonstroStrike/Player.h
+++ MonstroStrike/MonstroStrike/Player.h
@@ -1,28 +1,14 @@
 #pragma once
 #include "Utils.h"
-#include "CollisionShape.h" //To add AABB boxes
 
 struct Player {
 	Object obj;
 
 	bool isFacingRight;
-	bool onFloor; //Added to check entity on floor, hence can jump
 
 	AEVec2 expectedLocation;
-	AEVec2 velocity; //Added for movement - Johny
-	AEVec2 collisionNormal;
+
 	f32 lookAheadMutliplier;
-	
-	
-	//Gravity affection
-	f32 mass;
-	
-	//Collision boxes
-	AABB collisionBox;
-	AABB boxHeadFeet;
-	AABB boxArms;
-
-
 };
 
 Player* PlayerInitialize(const char* filename, AEVec2 scale, AEVec2 location, AEVec2 speed, bool isFacingRight); 
