61211682373cba28cea5cf1267ba04d0ebf82afc
penyew <penyew@hotmail.com>
2024-02-05 13:33:11 +0800

    Update Inventory.cpp

--- MonstroStrike/MonstroStrike/Inventory.cpp
+++ MonstroStrike/MonstroStrike/Inventory.cpp
@@ -130,31 +130,31 @@ namespace Inventory
 
 		}
 
-		Writer<Inventory> writer(inventory);
-
-		writer.StartObject();
-		writer.Key("hello");
-		writer.String("world");
-		writer.Key("t");
-		writer.Bool(true);
-		writer.Key("f");
-		writer.Bool(false);
-		writer.Key("n");
-		writer.Null();
-		writer.Key("i");
-		writer.Uint(123);
-		writer.Key("pi");
-		writer.Double(3.1416);
-		writer.Key("a");
-		writer.StartArray();
-		for (unsigned i = 0; i < 4; i++)
-			writer.Uint(i);
-		writer.EndArray();
-		writer.EndObject();
+		// Writer<Inventory> writer(inventory);
+		//
+		// writer.StartObject();
+		// writer.Key("hello");
+		// writer.String("world");
+		// writer.Key("t");
+		// writer.Bool(true);
+		// writer.Key("f");
+		// writer.Bool(false);
+		// writer.Key("n");
+		// writer.Null();
+		// writer.Key("i");
+		// writer.Uint(123);
+		// writer.Key("pi");
+		// writer.Double(3.1416);
+		// writer.Key("a");
+		// writer.StartArray();
+		// for (unsigned i = 0; i < 4; i++)
+		// 	writer.Uint(i);
+		// writer.EndArray();
+		// writer.EndObject();
 
 		//std::cout << inventory << std::endl;
 		
-		OStreamWrapper osw(ofs);
+		//OStreamWrapper osw(ofs);
 
 		// Writer<OStreamWrapper> writer(osw);
 		// json.Accept(writer);
